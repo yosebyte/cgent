ae9c4561db7628f6b7c73da157f3bab1aa6d7783
𝐘𝐨𝐬𝐞𝐛𝐲𝐭𝐞 <54016243+yosebyte@users.noreply.github.com>
2024-12-16 15:05:41 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 UUID=$(uuidgen)
-cat <<EOF > /config.yml
+cat <<EOF > /root/config.yml
 client_secret: ${SECRET}
 server: ${SERVER}
 tls: ${TLS}
 uuid: ${UUID}
 EOF
-exec /cgent
+exec /cgent -c=/root/config.yml
